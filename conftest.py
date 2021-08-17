d27d1d1b58358abc3b43d8b3654e4dc9070ce1a9
Oleh Romanyshyn <Oleh60@gmail.com>
2021-08-17 16:03:31 +0300

    First commit of this project

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome("C:\\CD\\chromedriver.exe")
+#     browser.maximize_window()
+#     browser.implicitly_wait(8)
+#     yield browser1
+#     print("\nquit browser..")
+#     browser1.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: en or others")
+
+#
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome("C:\\CD\\chromedriver.exe", options=options)
+    browser.implicitly_wait(8)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
