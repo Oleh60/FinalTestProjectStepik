25a4d433502c1b20e62245d9aa37265866c54d76
Oleh60 <82338987+Oleh60@users.noreply.github.com>
2021-08-17 17:09:42 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,10 @@
 
 class MainBase():
 
-    def __init__(self,browser,link):
-        self.browser = browser
-        self.link = link
 
     def test_guest_can_go_to_login_page(self,browser,link):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(self.link)
+    browser.get(link)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
