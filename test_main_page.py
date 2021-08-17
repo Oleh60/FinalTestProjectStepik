ce8414e1b293fe06a263d7ea5228b70dc7afd664
Oleh Romanyshyn <Oleh60@gmail.com>
2021-08-17 17:02:49 +0300

    Page Objects style new files #1

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,21 @@
-def test_guest_can_go_to_login_page(browser):
+
+class MainBase():
+
+    def __init__(self,browser,link):
+        self.browser = browser
+        self.link = link
+
+    def test_guest_can_go_to_login_page(self,browser,link):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    browser.get(self.link)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+    # def go_to_login_page(browser):
+    # login_link = browser.find_element_by_css_selector("#login_link")
+    # login_link.click()
+
+# def test_guest_can_go_to_login_page(browser):
+#    browser.get(link)
+#    go_to_login_page(browser)
