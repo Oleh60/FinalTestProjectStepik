6a6ed6743f8ab443543364d69c95cff76ac28a8b
Oleh Romanyshyn <Oleh60@gmail.com>
2021-08-24 21:47:53 +0300

    make a new locator for login page then tested how works tests of login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,21 +1,19 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
-
-<<<<<<< HEAD
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
     page.go_to_login_page()
-=======
 
-    def test_guest_can_go_to_login_page(self,browser,link):
+
+def test_guest_can_go_to_login_page(self, browser, link):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link = browser.find_element_by_CSS("#login_link")
     login_link.click()
 
->>>>>>> 25a4d433502c1b20e62245d9aa37265866c54d76
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -23,9 +21,25 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-<<<<<<< HEAD
-=======
+def test_correct_url_adress(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_registration_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_register_form()
+
+
 # def test_guest_can_go_to_login_page(browser):
 #    browser.get(link)
 #    go_to_login_page(browser)
->>>>>>> 25a4d433502c1b20e62245d9aa37265866c54d76
