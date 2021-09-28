bdc85ad8fc17a7d6066a5a025d9cd192f9224d35
Oleh Romanyshyn <Oleh60@gmail.com>
2021-09-28 17:00:40 +0300

    final fix on conftest,removed cromedriver location link

--- conftest.py
+++ conftest.py
@@ -20,7 +20,7 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart chrome browser for test..")
-    browser = webdriver.Chrome("C:\\CD\\chromedriver.exe", options=options)
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(8)
     yield browser
     print("\nquit browser..")
